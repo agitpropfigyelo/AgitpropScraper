6d59abe5f26c695c044fb7122f7caaad51af76e1
Fortyfei <fortyfei@outlook.com>
2024-02-29 17:21:25 +0100

    Add exceptions

---
+++ webscraper/EmptyCorpusException.cs
@@ -0,0 +1,19 @@
+﻿namespace webscraper;
+using System;
+
+public class EmptyCorpusException : Exception
+{
+    public EmptyCorpusException()
+    {
+    }
+
+    public EmptyCorpusException(string message)
+        : base(message)
+    {
+    }
+
+    public EmptyCorpusException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
