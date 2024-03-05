ab66b76839c90b524b3a73bc507d69ab498e8384
Fortyfei <fortyfei@outlook.com>
2024-03-05 23:19:46 +0100

    Update structure

--- webscraper/Program.cs
+++ webscraper/Program.cs
@@ -4,48 +4,59 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        List<string> sitesToScrape =
+        [
+            "ripost",
+            "mandiner",
+            "metropol",
+            "origo",
+            "magyarnemzet",
+            "pestisracok",
+            "magyarjelen",
+            "kuruczinfo",
+            "alfahir",
+            "24hu",
+            "444",
+            "telex",
+            "rtl",
+            "index",
+            "merce"
+        ];
+        List<IArchiveScraperService> archiveScrapers = ArchiveScraperFactory.GetScraperForSites(sitesToScrape);
+        Console.WriteLine("Start");
         //lekérni az összes cikket
         List<DateTime> datesToScrape = [
-                    // new DateTime(2024, 02, 10),
-                    // new DateTime(2001, 09, 11),
-                    // new DateTime(2015, 11, 14),
-                    // new DateTime(2015, 02, 06),
-                    new DateTime(2015, 02, 07),
+            new DateTime(2024, 02, 10),
+            // new DateTime(2001, 09, 11),
+            // new DateTime(2015, 11, 14),
+            // new DateTime(2015, 02, 06),
+            // new DateTime(2015, 02, 07),
             // new DateTime(2022, 02, 13),
             // new DateTime(2022, 02, 14),
             // new DateTime(2022, 02, 23),
         ];
-        List<IArchiveScraperService> archiveScrapers =
-        [
-            //TODO:
-            //new SitemapScraperArchive("https://ripost.hu/","ripost"),
-                //kormany
-            //new SitemapScraperArchive("https://mandiner.hu/","mandiner"),
-            //new SitemapScraperArchive("https://metropol.hu/","metropol"),
-            new OrigoArchiveScraper(),
-            //magyarnemzet
-            //pestisracok
-                //szelsojobb
-            //magyarjelen
-            //kuruczinfo
-            //alfahir
-                //libsi
-            //24hu
-            //444
-            //hvg
-            //telex
-            //rtl
-            //index
-                //bal
-            //merce
-        ];
+
         //létezik-e a cikk az adatbázisban?
 
         //feldolgozni az összes cikket
-
+        List<Article> articles = archiveScrapers.SelectMany(scraper => scraper.GetArticlesForDayAsync(datesToScrape[0]).Result).ToList();
         //ner-elés
-
+        foreach (Article article in articles)
+        {
+            System.Console.WriteLine("-----------------------------");
+            try
+            {
+                System.Console.WriteLine(article.Url);
+                IArticleScraperService scraper = ArticleScraperFactory.GetScraperForSite(article.Source);
+                article.Corpus=scraper.GetCorpus(article);
+                System.Console.WriteLine(article.Corpus);
+                
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("Failed");
+            }
+        }
         //beírni adatbázisba
-        Console.WriteLine("Hello, World!");
     }
 }
