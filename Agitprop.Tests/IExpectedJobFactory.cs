66f70aef4a1a8b24de52b7f5f7bb107c8d777b62
Fortyfei <fortyfei@outlook.com>
2024-12-18 21:11:02 +0100

    removed old tests

--- Agitprop.Tests/IExpectedJobFactory.cs
+++
@@ -1,9 +0,0 @@
-using Agitprop.Core;
-using Agitprop.Core.Enums;
-
-namespace NewsArticleScraper.Tests;
-
-public interface IExpectedJobFactory
-{
-    ScrapingJob GetPaginationJob(NewsSites source);
-}
