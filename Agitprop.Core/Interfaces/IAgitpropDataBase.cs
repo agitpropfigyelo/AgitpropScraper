6d9fb532e3756b2c8c9c25aa88e6d7091f98ccba
Fortyfei <fortyfei@outlook.com>
2024-12-30 18:30:53 +0100

    Removed leftovers

--- Agitprop.Core/Interfaces/IAgitpropDataBase.cs
+++
@@ -1,8 +0,0 @@
-using Agitprop.Core.Enums;
-
-namespace Agitprop.Core.Interfaces;
-
-public interface IAgitpropDataBaseService
-{
-    Task<int> CreateMentionsAsync(string url, ContentParserResult article, NamedEntityCollection entities);
-}
