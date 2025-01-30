83a89ad29ea8006a31383df436dd2a0261acd93d
Fortyfei <fortyfei@outlook.com>
2025-01-30 19:39:18 +0100

    Update Huszonnegy content parser with new XPath for article extraction and enhance test case with expected text content

--- Agitprop.Scraper.Sinks.Newsfeed/Scrapers/ContentParsers/HuszonnegyArticleContentParser.cs
+++ Agitprop.Scraper.Sinks.Newsfeed/Scrapers/ContentParsers/HuszonnegyArticleContentParser.cs
@@ -7,6 +7,6 @@ internal class HuszonnegyArticleContentParser : BaseArticleContentParser
     protected override List<string> DateXPaths => new List<string> { "//meta[@property='article:published_time']" };
     protected override List<string> TitleXPaths => new List<string> { "//h1[@class='o-post__title']" };
     protected override List<string> LeadXPaths => new List<string> { "//h1[@class='o-post__lead lead post-lead cf _ce_measure_widget']" };
-    protected override List<string> ArticleXPaths => new List<string> { "//div[@class='o-post__body post-body']", "//div[@class='article-body']" };
+    protected override List<string> ArticleXPaths => new List<string> { "//*[@id='content']/div/div[1]/div[2]/div[1]/div[2]/div[6]/div[1]" };
     protected override NewsSites SourceSite => NewsSites.Huszonnegy;
 }
