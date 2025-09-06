17a10955f4ef1ba1405cf1030ed7ac538465ae45
Fortyfei <fortyfei@outlook.com>
2025-09-06 13:31:22 +0200

    fix: Update EntityDto to handle null Id values and remove unused property

--- Agitprop.Web.API/Extensions.cs
+++ Agitprop.Web.API/Extensions.cs
@@ -14,7 +14,7 @@ internal static class Extensions
     {
         return new EntityDto
         {
-            Id = e.Id.ToString(),
+            Id = e.Id?.ToString()??"<empty>",
             Name = e.Name,
         };
     }
