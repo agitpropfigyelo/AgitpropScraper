40a3315a379ad8b920746c563387b2c00912c41e
Fortyfei <fortyfei@outlook.com>
2025-09-13 17:17:27 +0200

    feat: Implement constructor and dependencies in TrendingRepository

--- Agitprop.Infrastructure.SurrealDB/TrendingRepository.cs
+++ Agitprop.Infrastructure.SurrealDB/TrendingRepository.cs
@@ -1,10 +1,22 @@
 
 using Agitprop.Infrastructure.SurrealDB.Models;
 
+using Microsoft.Extensions.Logging;
+
+using SurrealDb.Net;
+
 namespace Agitprop.Infrastructure.SurrealDB;
 
 public class TrendingRepository : ITrendingRepository
 {
+    private readonly ISurrealDbClient _client;
+    private readonly ILogger<EntityRepository> _logger;
+
+    public TrendingRepository(ISurrealDbClient client, ILogger<EntityRepository> logger)
+    {
+        _client = client;
+        _logger = logger;
+    }
     public Task<IEnumerable<Entity>> GetTrendingEntitiesAsync(DateTime fromDate, DateTime toDate)
     {
         throw new NotImplementedException();
