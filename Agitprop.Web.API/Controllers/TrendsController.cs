85b6659255112361829a49cf67f7db970440063f
Fortyfei <fortyfei@outlook.com>
2025-10-16 17:21:54 +0200

    added tracing

--- Agitprop.Web.API/Controllers/TrendsController.cs
+++ Agitprop.Web.API/Controllers/TrendsController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using Agitprop.Core.Interfaces;
 
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +15,7 @@ public class TrendsController : ControllerBase
     private readonly ITrendingRepository _trendingRepository;
     private readonly IEntityRepository _entityRepository;
     private readonly ILogger<TrendsController> _logger;
+    private static readonly ActivitySource _activitySource = new("Agitprop.Web.Api.Controllers.TrendsController");
 
     public TrendsController(ITrendingRepository trendingRepository, ILogger<TrendsController> logger, IEntityRepository entityRepository)
     {
@@ -24,23 +27,25 @@ public class TrendsController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<TrendingResponse>> GetTrending([FromQuery] DateOnly from, [FromQuery] DateOnly to)
     {
+        using var activity = _activitySource.StartActivity("GetTrendingEntities", ActivityKind.Server);
         try
         {
             var trending = _trendingRepository.GetTrendingEntitiesAsync(from, to);
             var mentionings = _entityRepository.GetMentioningArticlesAsync(trending.Select(e => e.Id.ToString()), from, to);
-            
-            var result=trending.Select(e=>new EntityDetailsDto
+
+            var result = trending.Select(e => new EntityDetailsDto
             {
-                Id=e.Id,
-                Name=e.Name,
-                TotalMentions=mentionings[e.Id.ToString()].Count(),
-                MentionsCountByDate=mentionings[e.Id.ToString()].GroupBy(a=>DateOnly.FromDateTime(a.PublishedTime)).ToDictionary(g=>g.Key,g=>g.Count())
-            }).OrderByDescending(e=>e.TotalMentions).ToList();
+                Id = e.Id,
+                Name = e.Name,
+                TotalMentions = mentionings[e.Id.ToString()].Count(),
+                MentionsCountByDate = mentionings[e.Id.ToString()].GroupBy(a => DateOnly.FromDateTime(a.PublishedTime)).ToDictionary(g => g.Key, g => g.Count())
+            }).OrderByDescending(e => e.TotalMentions).ToList();
             return Ok(new { trending = result });
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "Error in GetTrendingEntities");
+            activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
             return StatusCode(500, new { error = "Internal server error." });
         }
     }
