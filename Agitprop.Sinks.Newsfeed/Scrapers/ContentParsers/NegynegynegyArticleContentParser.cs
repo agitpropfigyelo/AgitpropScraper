9494881bfd7d48b4f9f2c5584360cea98b9c4379
Fortyfei <fortyfei@outlook.com>
2025-10-30 20:35:41 +0100

    Align article scraping of 444 to current layout

--- Agitprop.Sinks.Newsfeed/Scrapers/ContentParsers/NegynegynegyArticleContentParser.cs
+++ Agitprop.Sinks.Newsfeed/Scrapers/ContentParsers/NegynegynegyArticleContentParser.cs
@@ -5,8 +5,8 @@ namespace Agitprop.Sinks.Newsfeed.Scrapers.ContentParsers;
 internal class NegynegynegyArticleContentParser : BaseArticleContentParser
 {
     protected override List<string> DateXPaths => new List<string> { "//meta[@property='article:published_time']" };
-    protected override List<string> TitleXPaths => new List<string> { "/html/body/div[1]/div[1]/div[3]/div[3]/h1" };
+    protected override List<string> TitleXPaths => new List<string> { "/html/body/div[1]/div[1]/div[4]/div[1]/h1" };
     protected override List<string> LeadXPaths => new List<string> { };
-    protected override List<string> ArticleXPaths => new List<string> { "/html/body/div[1]/div[1]/div[3]/div[5]/div/div[2]//p" };
+    protected override List<string> ArticleXPaths => new List<string> { "/html/body/div[1]/div[1]/div[4]/div[2]/div/div[3]//p" };
     protected override NewsSites SourceSite => NewsSites.NegyNegyNegy;
 }
